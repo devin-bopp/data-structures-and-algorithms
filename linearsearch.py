1407f145bff1bde539ba6c66892398752cfded78
devinrbopp <devinrbopp@gmail.com>
2022-02-25 11:11:03 -0700

    added duplicate search to linear search

--- linearsearch.py
+++ linearsearch.py
@@ -1,5 +1,6 @@
 from test_lists import test_list
 
+# linear search for finding the first instance
 def linear_search(li, target):
     for idx in range(len(li)):
         if li[idx] == target:
@@ -7,3 +8,17 @@ def linear_search(li, target):
     return -1
 
 print(linear_search(test_list, 700))
+
+def linear_dupe_search(li, target):
+    results = []
+
+    for idx in range(len(li)):
+        if li[idx] == target:
+            results.append(idx)
+    
+    if not results:
+        return 'not found'
+    else:
+        return results
+
+print(linear_dupe_search([1,2,3,4,5,2,1,1,1], 8))

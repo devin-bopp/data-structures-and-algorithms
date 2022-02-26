f164a73078f6c526593cd8f047606adbd8b8da88
devinrbopp <devinrbopp@gmail.com>
2022-02-25 19:31:53 -0700

    adding Node and LinkedList classes

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,5 +1,6 @@
+# recursive bubble sort
 def bubblesort(li):
-    # this is used to determine whether the list needs to reset
+    # used to determine whether the list is fully sorted
     swapped = False
     
     for idx in range(len(li)):
@@ -8,36 +9,12 @@ def bubblesort(li):
             li[idx] = li[idx + 1]
             li[idx + 1] = bookmark
             swapped = True
+    # if no swaps were performed, the list is sorted (base case)
     if not swapped:
         return li
+    # if the list is not sorted, recursively call bubblesort
     else:
-        swapped = False
         return bubblesort(li)
     
-
-import timeit
-
-my_setup = '''
-from test_lists import test_list
-def bubblesort(li):
-    # this is used to determine whether the list needs to reset
-    swapped = False
-    
-    for idx in range(len(li)):
-        if idx != len(li) - 1 and li[idx] > li[idx + 1]:
-            bookmark = li[idx]
-            li[idx] = li[idx + 1]
-            li[idx + 1] = bookmark
-            swapped = True
-    if not swapped:
-        return li
-    else:
-        swapped = False
-        return bubblesort(li)
-'''
-
-my_code = '''
-bubblesort([8,5,4,7,9,1])
-'''
-
-print(timeit.timeit(setup = my_setup, stmt = my_code, number = 1000000))
+sorted = bubblesort(['dog', 'cat', 'house', 'zebra', 'hen'])
+print(sorted)
